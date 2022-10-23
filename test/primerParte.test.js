d0bbbe31114da2f238962bc44f40057a9f99dd1b
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-23 19:26:33 -0300

    TDD iteration

--- test/primerParte.test.js
+++ test/primerParte.test.js
@@ -5,6 +5,7 @@ test("Prueba de posicion apagada inicialmente",()=>{
     expect(posicion.estadoActual()).toBe("Apagado");
 })
 
-test("Que no se puedan crear posiciones fuera del tablero"){
+test("Que no se puedan crear posiciones fuera del tablero",()=>{
+
     expect(()=>{var posicion= new Posicion(100,0);}).toThrow(new Error("No se puede crear una posicion con eje en x mayor a 99"));
-}
+})
