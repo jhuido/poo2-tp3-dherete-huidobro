dae118b88626f0d68ff75ec928aed91d3cd8f373
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-23 19:18:41 -0300

    TDD iteration

---
+++ main/Posicion.js
@@ -0,0 +1,8 @@
+function Posicion(posicionx,posiciony){
+    this.posicionx=posicionx;
+    this.posiciony=posciony;
+    this.estado="Apagado";
+    this.estadoActual=function(){
+        return this.estado;
+    }
+}
