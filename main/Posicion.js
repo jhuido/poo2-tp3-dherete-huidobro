3fc24cfd7e668ab1d20b8ef0679f16186e0b3e65
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-23 19:21:11 -0300

    TDD iteration

--- main/Posicion.js
+++ main/Posicion.js
@@ -1,6 +1,6 @@
 function Posicion(posicionx,posiciony){
     this.posicionx=posicionx;
-    this.posiciony=posciony;
+    this.posiciony=posiciony;
     this.estado="Apagado";
     this.estadoActual=function(){
         return this.estado;
--- test/primerParte.test.js
+++ test/primerParte.test.js
@@ -1,6 +1,6 @@
 const Posicion=require('../main/Posicion.js')
 
 test("Prueba de posicion apagada inicialmente",()=>{
-    var posicion=new Posicion();
+    var posicion=new Posicion(0,0);
     expect(posicion.estadoActual()).toBe("Apagado");
 })
