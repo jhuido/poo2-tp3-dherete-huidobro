2b2971a3a2a6ddb40234b4731b07f57f4a166c6e
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-24 07:41:30 -0300

    TDD iteration

--- test/primerParte.test.js
+++ test/primerParte.test.js
@@ -1,3 +1,4 @@
+const { default: expect } = require('expect');
 const Posicion=require('../main/Posicion.js')
 
 test("Prueba de posicion apagada inicialmente",()=>{
@@ -11,25 +12,7 @@ test("Que no se puedan crear posiciones fuera del tablero",()=>{
     expect(()=>{var posicion= new Posicion(0,100);}).toThrow(new Error("No se puede crear una posicion con eje en y mayor a 99"));
 })
 
-test("Prender rectangulo de luces",()=>{
+test("Crear tablero",()=>{
     var tablero= new Tablero();
-    var posicionVertice1=new Posicion(0,0);
-    var posicionVertice2=new Posicion(2,2);
-    tablero.encender(posicionVertice1,posicionVertice2)
-    expect(posicionVertice1.estadoActual()).toBe("Encendido");
-    expect(posicionVertice2.estadoActual()).toBe("Encendido");
-    var posicionAux=new Posicion(0,1);
-    expect(posicionAux.estadoActual()).toBe("Encendido");
-    var posicionAux=new Posicion(0,2);
-    expect(posicionAux.estadoActual()).toBe("Encendido");
-    var posicionAux=new Posicion(1,0);
-    expect(posicionAux.estadoActual()).toBe("Encendido");
-    var posicionAux=new Posicion(1,1);
-    expect(posicionAux.estadoActual()).toBe("Encendido");
-    var posicionAux=new Posicion(1,2);
-    expect(posicionAux.estadoActual()).toBe("Encendido");
-    var posicionAux=new Posicion(2,0);
-    expect(posicionAux.estadoActual()).toBe("Encendido");
-    var posicionAux=new Posicion(2,1);
-    expect(posicionAux.estadoActual()).toBe("Encendido");
+    expect(tablero.crearTablero()).toBe("Tablero Creado");
 })
