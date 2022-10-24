296d0a1eddc7aa3a98e59a46a51384b4b963ac9f
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-24 07:43:33 -0300

    TDD iteration

--- main/Tablero.js
+++ main/Tablero.js
@@ -1,7 +1,15 @@
 const Posicion = require("./Posicion");
 
 function Tablero(){
-    
+    this.tablero=[];
+    this.crearTablero=function(){
+        for(i=0;i<99;i++){
+            for(j=0;j<99;j++){
+                this.tablero[i,j]=new Posicion(i,j);
+            }
+        }
+        return "Tablero creado";
+    }
 }
 
 module.exports=Tablero;
