79f0713ffa086a04afcacde9821637ca4dc83e29
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-24 09:39:53 -0300

    TDD iteration

--- main/Tablero.js
+++ main/Tablero.js
@@ -32,7 +32,7 @@ function Tablero(){
     this.cambiar=function(verticeSuperior,verticeInferior){
         var i=verticeSuperior.posiciony;
         var j=verticeSuperior.posicionx;
-        console.log(i+"´,"+j);
+        console.log(this.tablero[0,0].estadoActual());
         for(i;i<((verticeInferior.posiciony)+1);i++){
             for(j;j<((verticeInferior.posicionx)+1);j++){
 
