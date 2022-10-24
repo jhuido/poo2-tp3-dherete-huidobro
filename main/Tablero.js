9f4b5ba1d0df1fd92017d6ae8e385c76f732da25
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-24 10:27:47 -0300

    TDD iteration

--- main/Tablero.js
+++ main/Tablero.js
@@ -34,7 +34,7 @@ function Tablero(){
         for(i=verticeSuperior.posiciony;i<=verticeInferior.posiciony;i++){
             for(j=verticeSuperior.posicionx;j<=verticeInferior.posicionx;j++){
                 estado=this.tablero[i,j].estadoActual();
-                if(estado=="Encendido"){
+                if(estado==="Encendido"){
                     this.tablero[i,j].cambiarEstado("Apagado");
                 }
                 else{
