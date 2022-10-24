30b68b98e34aed4d328c64f34e5b18b56828c35b
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-24 09:13:49 -0300

    TDD iteration

--- test/primerParte.test.js
+++ test/primerParte.test.js
@@ -73,12 +73,10 @@ test("Cambiar rectangulo de luces",()=>{
     tablero.crearTablero();
 
     tablero.encender(verticeSuperior,verticeInferior);
-    expect(tablero.tablero[0,0].estadoActual()).toBe("Encendido");
     var verticeSup2=new Posicion(0,1);
     var verticeInf2=new Posicion(2,2);
 
     tablero.cambiar(verticeSup2,verticeInf2);
-
     expect(tablero.tablero[0,0].estadoActual()).toBe("Encendido");
     expect(tablero.tablero[1,0].estadoActual()).toBe("Encendido");
     expect(tablero.tablero[2,0].estadoActual()).toBe("Encendido");
